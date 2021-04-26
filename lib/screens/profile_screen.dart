0e9163ad0f82b4563ead222cf880f9eebfc80617
Gokul Kannan <gokulskannan1993@gmail.com>
2021-04-26 23:10:52 +0100

    profile screen interpolation complete with async problem solved and no error screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-import 'dart:math';
-
 import 'package:babysitter_booking_app/screens/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +25,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
   //fire auth instance
   final _auth = FirebaseAuth.instance;
   User loggedInUser;
-  DocumentSnapshot userData;
 
   @override
   void initState() {
@@ -37,22 +33,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   //checks for logged in user
-  void getCurrentUser() async {
+  void getCurrentUser() {
     try {
       final user = _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
-        userData =
-            await _firestore.collection("users").doc(loggedInUser.uid).get();
-        username = userData["name"];
-        role = userData["role"];
-        email = userData["email"];
-        phone = userData["phone"];
-        about = userData["about"];
-        location = userData["location"];
-        followers = userData["followers"];
-        recommends = userData["recommends"];
-        rating = userData["rating"];
       }
     } catch (e) {
       print(e);
@@ -62,121 +47,143 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        Container(
-          color: kSecondaryColor,
-          height: MediaQuery.of(context).size.height * 0.3,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                Stack(children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: NetworkImage(
-                        "https://st3.depositphotos.com/15648834/17930/v/1600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"),
+      body: FutureBuilder(
+        // so that to eliminate the need of hot reload due to async
+        future: _firestore.collection("users").doc(loggedInUser.uid).get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            // if the snapshot is empty
+            return Text("Loading...");
+          } else {
+            //Mapping all the fields
+            Map<String, dynamic> userData = snapshot.data.data();
+            username = userData["name"];
+            role = userData["role"];
+            email = userData["email"];
+            phone = userData["phone"];
+            about = userData["about"];
+            location = userData["location"];
+            followers = userData["followers"];
+            recommends = userData["recommends"];
+            rating = userData["rating"];
+            return ListView(children: [
+              Container(
+                color: kSecondaryColor,
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Stack(children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                              "https://st3.depositphotos.com/15648834/17930/v/1600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"),
+                        ),
+                        Positioned(
+                            bottom: 3,
+                            right: 2,
+                            child: CircleAvatar(
+                                backgroundColor: kPrimaryColor,
+                                radius: 15,
+                                child: (Icon(Icons.edit)))),
+                      ]),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        username,
+                        style: TextStyle(fontSize: 16, color: kPrimaryColor),
+                      ),
+                      Text(
+                        role,
+                        style: TextStyle(fontSize: 16, color: kTextColorLight),
+                      ),
+                    ],
                   ),
-                  Positioned(
-                      bottom: 3,
-                      right: 2,
-                      child: CircleAvatar(
-                          backgroundColor: kPrimaryColor,
-                          radius: 15,
-                          child: (Icon(Icons.edit)))),
-                ]),
-                SizedBox(
-                  height: 10,
                 ),
-                Text(
-                  username,
-                  style: TextStyle(fontSize: 16, color: kPrimaryColor),
+              ),
+              Card(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Text("User Rating"),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(rating),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("Followers"),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(followers),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("Recommends"),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(recommends),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                Text(
-                  role,
-                  style: TextStyle(fontSize: 16, color: kTextColorLight),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Card(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
+              ),
+              Container(
+                child: Column(
                   children: [
-                    Text("User Rating"),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(rating),
+                    Text("User Information"),
+                    Divider(),
+                    ListTile(
+                      title: Text(
+                        "Personal Information",
+                        style: TextStyle(color: kSecondaryColor),
+                      ),
+                      subtitle: Text(about),
+                      leading: Icon(Icons.info),
+                    ), // personal info
+                    ListTile(
+                      title: Text(
+                        "Location",
+                        style: TextStyle(color: kSecondaryColor),
+                      ),
+                      subtitle: Text(location),
+                      leading: Icon(Icons.location_on),
+                    ), // location info
+                    ListTile(
+                      title: Text(
+                        "Email",
+                        style: TextStyle(color: kSecondaryColor),
+                      ),
+                      subtitle: Text(email),
+                      leading: Icon(Icons.email),
+                    ), // email
+                    ListTile(
+                      title: Text(
+                        "Phone",
+                        style: TextStyle(color: kSecondaryColor),
+                      ),
+                      subtitle: Text(phone),
+                      leading: Icon(Icons.phone),
+                    ), // phone
                   ],
                 ),
-                Column(
-                  children: [
-                    Text("Followers"),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(followers),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text("Recommends"),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(recommends),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-        Container(
-          child: Column(
-            children: [
-              Text("User Information"),
-              Divider(),
-              ListTile(
-                title: Text(
-                  "Personal Information",
-                  style: TextStyle(color: kSecondaryColor),
-                ),
-                subtitle: Text(about),
-                leading: Icon(Icons.info),
-              ), // personal info
-              ListTile(
-                title: Text(
-                  "Location",
-                  style: TextStyle(color: kSecondaryColor),
-                ),
-                subtitle: Text(location),
-                leading: Icon(Icons.location_on),
-              ), // location info
-              ListTile(
-                title: Text(
-                  "Email",
-                  style: TextStyle(color: kSecondaryColor),
-                ),
-                subtitle: Text(email),
-                leading: Icon(Icons.email),
-              ), // email
-              ListTile(
-                title: Text(
-                  "Phone",
-                  style: TextStyle(color: kSecondaryColor),
-                ),
-                subtitle: Text(phone),
-                leading: Icon(Icons.phone),
-              ), // phone
-            ],
-          ),
-        ),
-      ]),
+              ),
+            ]);
+          }
+        },
+      ),
     );
   }
 }
